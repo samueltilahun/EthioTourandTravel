2a631c524a45f92055f503ca4f02d9a268a78577
samueltilahun <samueltilahun991@gmail.com>
2024-06-10 03:07:08 +0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require('express');
 const mongoose = require("mongoose"); 
 const path = require('path')
+const cors = require('cors');
 
 const app = express();
 
