a19b5c6b0e6cb363456fa1f57d271a59ab832b41
samueltilahun <samueltilahun991@gmail.com>
2024-06-14 11:38:47 +0300

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,8 +5,8 @@ import Navbar from './Components/Navbar/Navbar'
 import Main from './Components/Main/Main'
 import Footer from './Components/Footer/Footer'
 import Admin from './Components/Admin/admin'
-import Packages from './Components/Contact/Contact'
-import Packages from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import About from './Components/About/About'
 import Packages from './Components/Packages/Packages'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -27,8 +27,8 @@ const App = () => {
       <Route path="/" element={<CombinedLayout />}/>
       <Route path='/admin' element={<Admin />}/>
       <Route path='/packages' element={<Packages />}/>
-      <Route path='/contact' element={<Packages />}/>
-      <Route path='/about' element={<Packages />}/>
+      <Route path='/contact' element={<Contact />}/>
+      <Route path='/about' element={<About />}/>
     </Routes>
     <Footer />
     </BrowserRouter>
